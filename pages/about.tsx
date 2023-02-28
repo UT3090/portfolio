09125b642310ca9e2dcac95117b942e295bd54d3
osawa-koki <osawa-koki@example.com>
2023-02-28 13:01:20 +0900

    About

--- pages/about.tsx
+++ pages/about.tsx
@@ -1,11 +1,11 @@
 import Layout from "../components/Layout";
 
-export default function HelloWorld() {
+export default function AboutPage() {
   return (
     <Layout>
       <div id="About" className="mt-3">
-        <h1>Here, About page.</h1>
-        <p>Next.js大好きです。<br />本当に使いやすいです。<br />僕はTypeScript × Next.jsを強くオススメします。<br /><br />静的型付け言語って難しいと思われがちですが、エディタの機能が豊富な現在は型付け言語の方が簡単に書くことができます。<br />さらには字句解析と構文解析に加えて、コンパイル時に、コンパイラによる意味解析を行ってくれるため、動的型付け言語と比較して動作に信頼性があります。<br /><br />また、Reactと比べてNext.jsはSSGによってビルド時にHTMLが生成されるため、SPA(SSR)と比べて高速に動作し、ユーザビリティが高いです。</p>
+        <h1>About Kurokawa, Who is</h1>
+        <p>私は天才プログラマ、黒川唯人。<br /><br />ITのすべてを知り尽くした男。<br />私の辞書に不可能の二文字はない。</p>
       </div>
     </Layout>
   );
