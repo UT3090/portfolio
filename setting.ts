ad0637a891a1dd4c1558f62fe3acb6b31ebbe099
osawa-koki <osawa-koki@example.com>
2023-02-28 12:57:20 +0900

    小さな修正。

--- setting.ts
+++ setting.ts
@@ -4,10 +4,9 @@ const Env = require('./next.config.js');
 const isProd = process.env.NODE_ENV === 'production';
 
 const Setting = {
-  title: '🐟 nextjs-template.modern 🐟',
+  title: '🐟 Kurokawa Resume 🐟',
   isProd,
   basePath: Env.basePath,
-  apiPath: isProd ? '' : 'http://localhost:80',
 };
 
 export default Setting;
