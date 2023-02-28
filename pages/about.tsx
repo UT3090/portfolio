13a8eb838d41c319757e64f0e88a39fc8e1725ed
osawa-koki <osawa-koki@example.com>
2023-02-28 13:06:06 +0900

    Splide入れた。

--- pages/about.tsx
+++ pages/about.tsx
@@ -1,3 +1,4 @@
+import { Splide, SplideSlide } from '@splidejs/react-splide';
 import Layout from "../components/Layout";
 
 export default function AboutPage() {
@@ -6,6 +7,14 @@ export default function AboutPage() {
       <div id="About" className="mt-3">
         <h1>About Kurokawa, Who is</h1>
         <p>私は天才プログラマ、黒川唯人。<br /><br />ITのすべてを知り尽くした男。<br />私の辞書に不可能の二文字はない。</p>
+        <Splide>
+          <SplideSlide>
+            <img src="image1.jpg" alt="Image 1"/>
+          </SplideSlide>
+          <SplideSlide>
+            <img src="image2.jpg" alt="Image 2"/>
+          </SplideSlide>
+        </Splide>
       </div>
     </Layout>
   );
